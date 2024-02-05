3d168f0ccea2cdfd9c3867314c69b2460f6c9f09
lohcy <loh.cheeyung96@gmail.com>
2024-02-05 17:04:27 +0800

    create question_paper_models

---
+++ lib/models/question_paper_model.dart
@@ -0,0 +1,65 @@
+class AnswerModel {
+  String identifier;
+  String answer;
+
+  AnswerModel({required this.identifier, required this.answer});
+
+  factory AnswerModel.fromJson(Map<String, dynamic> json) {
+    return AnswerModel(
+      identifier: json["identifier"],
+      answer: json["Answer"],
+    );
+  }
+}
+
+class QuestionModel {
+  String id;
+  String question;
+  List<AnswerModel> answers;
+  String? correctAnswer;
+
+  QuestionModel({
+    required this.id,
+    required this.question,
+    required this.answers,
+    this.correctAnswer,
+  });
+
+  factory QuestionModel.fromJson(Map<String, dynamic> json) {
+    return QuestionModel(
+      id: json["id"] as String,
+      question: json["question"] as String,
+      answers: (json["answers"] as List).map((e) => AnswerModel.fromJson(e)).toList(),
+      correctAnswer: json["correct_answer"] as String,
+    );
+  }
+}
+
+class QuestionPaperModel {
+  String? id;
+  String? title;
+  String? image_url;
+  String? description;
+  String? time_seconds;
+  List<QuestionModel>? questions;
+
+  QuestionPaperModel({
+    this.id,
+    this.title,
+    this.image_url,
+    this.description,
+    this.time_seconds,
+    this.questions,
+  });
+
+  factory QuestionPaperModel.fromJson(Map<String, dynamic> json) {
+    return QuestionPaperModel(
+      id: json["id"] as String,
+      title: json["title"] as String,
+      image_url: json["image_url"] as String,
+      description: json["Description"] as String,
+      time_seconds: json["time_seconds"] as String,
+      questions: (json["questions"] as List).map((dynamic e) => QuestionModel.fromJson(e as Map<String, dynamic>)).toList(),
+    );
+  }
+}
