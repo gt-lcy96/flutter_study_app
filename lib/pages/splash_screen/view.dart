bfca7079003ef9bdb64c4300ea213d74be85fe23
lohcy <loh.cheeyung96@gmail.com>
2024-02-06 16:14:53 +0800

    init splash_screen with getX

---
+++ lib/pages/splash_screen/view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:study_app/pages/splash_screen/controller.dart';
+
+class SplashScreen extends GetView<SplashScreenController> {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
