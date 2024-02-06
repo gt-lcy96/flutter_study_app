43fa49e9e203b9c40e9beaa0dda14d7082018853
lohcy <loh.cheeyung96@gmail.com>
2024-02-06 16:02:38 +0800

    batch commit answers

--- lib/pages/data_uploader/controller.dart
+++ lib/pages/data_uploader/controller.dart
@@ -38,8 +38,25 @@ class DataUploaderController extends GetxController {
         "image_url": paper.image_url,
         "description": paper.description,
         "time_seconds": paper.time_seconds,
-        "questions_count": paper.questions == null ? 0 : paper.questions?.length,
+            paper.questions == null ? 0 : paper.questions?.length,
       });
+
+      for (var questions in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id!, questionId: questions.id);
+        batch.set(questionPath, {
+          "question": questions.question,
+          "correct_answer": questions.correctAnswer,
+        });
+
+        for (var answer in questions.answers) {
+          batch.set(
+              questionPath.collection("answers").doc(answer.identifier), {
+                "identifier": answer.identifier,
+                "answer": answer.answer,
+              });
+        }
+      }
     }
 
     batch.commit();
