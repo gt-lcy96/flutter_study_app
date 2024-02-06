4d4a9107e1526d2bd8fb3ff7a6226c0b1fe8d8ab
lohcy <loh.cheeyung96@gmail.com>
2024-02-06 15:05:15 +0800

    firebase batch.commit questionPaper

--- lib/pages/data_uploader/controller.dart
+++ lib/pages/data_uploader/controller.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:study_app/firebase_ref/references.dart';
 import 'package:study_app/models/question_paper_model.dart';
 
 class DataUploaderController extends GetxController {
@@ -12,7 +14,7 @@ class DataUploaderController extends GetxController {
   }
 
   Future<void> uploadData() async {
-    print('uploaded');
+    final fireStore = FirebaseFirestore.instance;
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
         .loadString("AssetManifest.json");
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
@@ -27,5 +29,19 @@ class DataUploaderController extends GetxController {
           .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
     }
     print('Items number ${questionPapers[2].title}');
+
+    var batch = fireStore.batch();
+
+    for (var paper in questionPapers) {
+      batch.set(questionPaperRF.doc(paper.id), {
+        "title": paper.title,
+        "image_url": paper.image_url,
+        "description": paper.description,
+        "time_seconds": paper.time_seconds,
+        "questions_count": paper.questions == null ? 0 : paper.questions?.length,
+      });
+    }
+
+    batch.commit();
   }
 }
