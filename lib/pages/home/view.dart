45662b86bc2a81fa69451adc763ac010d9b2a051
lohcy <loh.cheeyung96@gmail.com>
2024-02-09 15:07:21 +0800

    apply AppIcons.menuLeft icon

--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:study_app/configs/themes/app_icons.dart';
 import 'package:study_app/configs/themes/ui_parameters.dart';
 import 'package:study_app/controllers/question_paper_controller.dart';
 import 'package:study_app/pages/home/widgets/question_card.dart';
@@ -13,20 +14,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     QuestionPaperController _questionPaperController = Get.find();
     return Scaffold(
-      body: ContentArea(
-        addPadding: false,
-        child: Obx(
-          () => ListView.separated(
-            padding: UIParameters.mobileScreenPadding,
-            itemBuilder: (BuildContext context, int index) {
-              // print("_questionPaperController.allPapers[index].image_url!:  ${_questionPaperController.allPapers[index].image_url!}");
-              return QuestionCard(model: _questionPaperController.allPapers[index]);
-            },
-            separatorBuilder: (BuildContext context, int index) => SizedBox(
-              height: 20.h,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(mobileScreenPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(AppIcons.menuLeft, color: Colors.red),
+                ],
+              ),
             ),
-            itemCount: _questionPaperController.allPapers.length,
-          ),
+            Expanded(
+              child: ContentArea(
+                addPadding: false,
+                child: Obx(
+                  () => ListView.separated(
+                    padding: UIParameters.mobileScreenPadding,
+                    itemBuilder: (BuildContext context, int index) {
+                      // print("_questionPaperController.allPapers[index].image_url!:  ${_questionPaperController.allPapers[index].image_url!}");
+                      return QuestionCard(
+                          model: _questionPaperController.allPapers[index]);
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        SizedBox(
+                      height: 20.h,
+                    ),
+                    itemCount: _questionPaperController.allPapers.length,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
