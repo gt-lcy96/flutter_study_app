300af2f575dbd635486a16dfd4dbe58be7c0e82b
lohcy <loh.cheeyung96@gmail.com>
2024-02-09 16:41:02 +0800

    add back button to toggle ZoomDrawer

--- lib/pages/home/widgets/menu_screen.dart
+++ lib/pages/home/widgets/menu_screen.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:get/get.dart';
 import 'package:study_app/configs/themes/app_colors.dart';
+import 'package:study_app/configs/themes/ui_parameters.dart';
+import 'package:study_app/controllers/zoom_drawer_controller.dart';
 
-class MenuScreen extends StatelessWidget {
+class MenuScreen extends GetView<MyZoomDrawerController> {
   const MenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.maxFinite,
-      decoration: BoxDecoration(gradient: mainGradient()),
-    );
+        padding: UIParameters.mobileScreenPadding,
+        width: double.maxFinite,
+        decoration: BoxDecoration(gradient: mainGradient()),
+        child: Theme(
+          data: ThemeData(
+              textButtonTheme: TextButtonThemeData(
+                  style: TextButton.styleFrom(primary: onSurfaceTextColor))),
+          child: SafeArea(
+              child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                right: 0,
+                  child: BackButton(
+                color: Colors.white,
+                onPressed: () {
+                  controller.toggleDrawer();
+                },
+              ))
+            ],
+          )),
+        ));
   }
 }
