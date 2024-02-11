2e701187b75b00eff1afba219c77334b5a194336
lohcy <loh.cheeyung96@gmail.com>
2024-02-11 18:05:36 +0800

    create AuthController.signInWithGoogle

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,19 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:study_app/routes/routes.dart';
 
 class AuthController extends GetxController {
+  late FirebaseAuth _auth;
+  final _user = Rxn<User>();
+  late Stream<User?> _authStateChanges;
+
   @override
   void onReady() {
     // TODO: implement onReady
-    super.onReady();
     initAuth();
+    super.onReady();
   }
 
   Future<void> initAuth() async {
     await Future.delayed(const Duration(seconds: 2));
+    _auth = FirebaseAuth.instance;
+    _authStateChanges = _auth.authStateChanges();
+    _authStateChanges.listen((User? user) {
+      _user.value = user;
+    });
     navigateToIntroduction();
   }
   
+  signInWithGoogle() async {
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    try {
+      GoogleSignInAccount? account = await _googleSignIn.signIn();
+      if(account != null) {
+        final _authAccount = await account.authentication;
+        final _credential = GoogleAuthProvider.credential(
+          idToken: _authAccount.idToken,
+          accessToken: _authAccount.accessToken,
+        );
+
+        await _auth.signInWithCredential(_credential);
+        await saveUser(account);
+      }
+    } catch(e) {
+      print("Exception in auth_controller: $e");
+    }
+  }
   void navigateToIntroduction() {
     Get.offAllNamed(AppRoutes.INTRODUCTION);
   }
