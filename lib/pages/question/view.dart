8106155bbf2cad919fb33a2eb0952d41d799569b
lohcy <loh.cheeyung96@gmail.com>
2024-02-13 17:35:02 +0800

    create custom background

--- lib/pages/question/view.dart
+++ lib/pages/question/view.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:study_app/widgets/common/background_decoration.dart';
 
 class QuestionPage extends StatelessWidget {
   const QuestionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("I am question page"),
-      ),
+    return Scaffold(
+      body: BackgroundDecoration(child: Center(child: Text("I am here"))),
     );
   }
 }
