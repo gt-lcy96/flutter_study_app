66ae66209091a8e4ca1cdf979d5de20af4b841d5
lohcy <loh.cheeyung96@gmail.com>
2024-02-14 15:01:42 +0800

    update styling for question view

--- lib/configs/themes/custom_textStyle.dart
+++ lib/configs/themes/custom_textStyle.dart
@@ -10,6 +10,7 @@ TextStyle cartTitles(context) => TextStyle(
       fontWeight: FontWeight.bold,
     );
 
+const questionText = TextStyle(fontSize: 16, fontWeight: FontWeight.w800);
 const detailText = TextStyle(fontSize: 12);
 const headerText = TextStyle(
     fontSize: 22, fontWeight: FontWeight.w700, color: onSurfaceTextColor);
--- lib/pages/question/view.dart
+++ lib/pages/question/view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:study_app/configs/themes/custom_textStyle.dart';
 import 'package:study_app/firebase_ref/loading_status.dart';
 import 'package:study_app/pages/question/controller.dart';
 import 'package:study_app/widgets/common/background_decoration.dart';
 import 'package:study_app/widgets/common/question_place_holder.dart';
+import 'package:study_app/widgets/content_area.dart';
 
 class QuestionPage extends GetView<QuestionController> {
   const QuestionPage({super.key});
@@ -13,22 +15,28 @@ class QuestionPage extends GetView<QuestionController> {
     return Scaffold(
       body: BackgroundDecoration(
         child: Obx(() => Container(
-              height: Get.height,
-              width: Get.width,
+              // height: Get.height,
+              width: double.maxFinite,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   (controller.loadingStatus == LoadingStatus.completed)
                       ? Expanded(
+                          child: ContentArea(
                           child: SingleChildScrollView(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(controller.currentQuestion.value!.question),
-                            ],
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                    controller.currentQuestion.value!.question,
+                                    style: questionText,
+                                    ),
+                              ],
+                            ),
                           ),
                         ))
-                      : Expanded(child: QuestionScreenHolder()),
+                      : Expanded(
+                          child: ContentArea(child: QuestionScreenHolder())),
                 ],
               ),
             )),
