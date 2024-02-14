bcd13c12cf2a0767573305f2feb8168265a898b4
lohcy <loh.cheeyung96@gmail.com>
2024-02-14 15:51:02 +0800

    create answerSelectedColor and answerBorderColor

--- lib/configs/themes/app_colors.dart
+++ lib/configs/themes/app_colors.dart
@@ -1,3 +1,4 @@
+import 'package:get/get.dart';
 import 'package:study_app/configs/themes/ui_parameters.dart';
 
 import 'app_light_theme.dart';
@@ -22,5 +23,13 @@ LinearGradient mainGradient() =>
 Color customScaffoldColor(BuildContext context) => UIParameters.isDarkMode()
     ? const Color(0xFF2e3c62)
     : const Color.fromARGB(255, 240, 237, 255);
-    // ? const Color.fromARGB(255, 20, 46, 158)
-    // : const Color.fromARGB(255, 221, 221, 221);
+// ? const Color.fromARGB(255, 20, 46, 158)
+// : const Color.fromARGB(255, 221, 221, 221);
+
+Color answerSelectedColor() => UIParameters.isDarkMode()
+    ? Theme.of(Get.context!).cardColor.withOpacity(0.5)
+    : Theme.of(Get.context!).primaryColor;
+
+Color answerBorderColor() => UIParameters.isDarkMode()
+    ? const Color.fromARGB(255, 20, 46, 158)
+    : const Color.fromARGB(255, 221, 221, 221);
